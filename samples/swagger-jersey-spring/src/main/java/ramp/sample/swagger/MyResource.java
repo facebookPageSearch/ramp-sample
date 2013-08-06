45e8daa339ec62d350c842a0434a6b830cb641d4
rpalaniappan <rpalaniappan@intuit.com>
2013-08-06 22:39:13 +0530

    Swagger first checkin

--- samples/swagger-jersey-spring/src/main/java/ramp/sample/swagger/MyResource.java
+++ samples/swagger-jersey-spring/src/main/java/ramp/sample/swagger/MyResource.java
@@ -4,6 +4,9 @@ package ramp.sample.swagger;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import ramp.sample.swagger.types.User;
 
 /** Example resource class hosted at the URI path "/myresource"
  */
@@ -18,5 +21,16 @@ public class MyResource {
     @Produces("text/plain")
     public String getIt() {
         return "Hi there!";
+    }
+    
+    @GET
+    @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+    @Path("user")
+    public User getUser() {
+    	User user = new User();
+    	user.setEmailId("test.email.id@i.com");
+    	user.setFirstName("fName");
+    	user.setLastName("lName");
+    	return user;
     }
 }
