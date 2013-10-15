ea017ef74ef322fac28ec07c728f966dc1902fa1
rpalaniappan <rpalaniappan@intuit.com>
2013-10-15 16:43:36 +0530

    get it from http header and populate MDC

--- samples/log4jmdc/src/main/java/ramp/sample/log4jmdc/AppFilter.java
+++ samples/log4jmdc/src/main/java/ramp/sample/log4jmdc/AppFilter.java
@@ -11,6 +11,8 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -24,33 +26,50 @@ public class AppFilter implements Filter {
 
 	private static final Log LOG = LogFactory.getLog(AppFilter.class);
 
-	/**
-	 * 
-	 */
-	public AppFilter() {
-	}
 
 	public void destroy() {
 	}
 
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
 	private enum MdcKey {
-		TID("tid"), TRAFFIC_TYPE("trafficType");
+		TID("tid", "intuit_tid"), 
+		APP_NAME("appName", "intuit_app_name"),
+		TRAFFIC_TYPE("trafficType","Traffic-Type");
 
-		String key = null;
+		String mdcKey = null;
+		String httpHeaderName = null;
 
-		MdcKey(String key) {
-			this.key = key;
+		MdcKey(String key, String httpHeaderName) {
+			this.mdcKey = key;
+			this.httpHeaderName = httpHeaderName;
 		}
 
 		public String key() {
-			return key;
+			return mdcKey;
+		}
+		public String getHttpHeader() {
+			return httpHeaderName;
 		}
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
+		if (request instanceof HttpServletRequest
+				&& response instanceof HttpServletResponse) {
+			doFilter((HttpServletRequest) request,
+					(HttpServletResponse) response, chain);
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	private void doFilter(HttpServletRequest request,
+			HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
 		try {
-			addMdc("12345678901234567", "LIVE");
+			addMdc(request);
 			long start = System.currentTimeMillis();
 			chain.doFilter(request, response);
 			LOG.info("processingTime=" + (System.currentTimeMillis() - start));
@@ -59,18 +78,28 @@ public class AppFilter implements Filter {
 		}
 	}
 
-	public void init(FilterConfig arg0) throws ServletException {
-	}
-
-	//To log these values, add these in your appender's ConversionPattern
-	// %X{tid} %X{trafficType}
-	//Example logs
-	//2013-10-15 15:03:59,217 +0530 level=ERROR class=ramp.sample.log4jmdc.HelloWorldServlet tid=12345678901234567 trafficType=LIVE              Error Message
-	//2013-10-15 15:03:59,218 +0530 level=INFO  class=ramp.sample.log4jmdc.AppFilter tid=12345678901234567 trafficType=LIVE              processingTime=1004
-	private void addMdc(String tidValue, String trafficTypeValue) {
-		MDC.put(MdcKey.TID.key(), MdcKey.TID.key() + "=" + tidValue);
-		MDC.put(MdcKey.TRAFFIC_TYPE.key(), MdcKey.TRAFFIC_TYPE.key() + "="
-				+ trafficTypeValue);
+	// To log these values, add these in your appender's ConversionPattern
+	// %X{tid} %X{appName} %X{trafficType}
+	// Sample logs
+	// 2013-10-15 16:37:32,216 +0530 level=INFO
+	// class=ramp.sample.log4jmdc.HelloWorldServlet tid=sample-tid
+	// appName=sample-app trafficType=SANDBOX Info message
+	// 2013-10-15 16:37:32,217 +0530 level=WARN
+	// class=ramp.sample.log4jmdc.HelloWorldServlet tid=sample-tid
+	// appName=sample-app trafficType=SANDBOX Warning Message
+	// 2013-10-15 16:37:33,218 +0530 level=ERROR
+	// class=ramp.sample.log4jmdc.HelloWorldServlet tid=sample-tid
+	// appName=sample-app trafficType=SANDBOX Error Message
+	// 2013-10-15 16:37:33,219 +0530 level=INFO
+	// class=ramp.sample.log4jmdc.AppFilter tid=sample-tid appName=sample-app
+	// trafficType=SANDBOX processingTime=1004
+	private void addMdc(HttpServletRequest request) {
+		for (MdcKey mdcKey : MdcKey.values()) {
+			String value = request.getHeader(mdcKey.getHttpHeader());
+			if (mdcKey != null && mdcKey.key() != null && value != null) {
+				MDC.put(mdcKey.key(), mdcKey.key() + "=" + value);
+			}
+		}
 	}
 
 	private void clearMdc() {
