9b8767c4df239dff16690951bff5de2f83fabad9
rpalaniappan <rpalaniappan@intuit.com>
2014-01-08 23:38:16 +0530

    jdbc template

--- samples/swagger-jersey-spring/src/main/java/ramp/sample/swagger/MyResource.java
+++ samples/swagger-jersey-spring/src/main/java/ramp/sample/swagger/MyResource.java
@@ -6,11 +6,15 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+
 import ramp.sample.swagger.types.User;
 
-/** Example resource class hosted at the URI path "/myresource"
+/** Example resource class hosted at the URI path "/api"
  */
 @Path("/myresource")
+@Api(value = "/myresource", description = "Test API")
 public class MyResource {
     
     /** Method processing HTTP GET requests, producing "text/plain" MIME media
@@ -19,13 +23,25 @@ public class MyResource {
      */
     @GET 
     @Produces("text/plain")
+    @ApiOperation(
+            value = "Simple get method",
+            notes = "Hello World equivalent",
+            tags = "get",
+            httpMethod = "GET",
+            responseClass = "java.lang.String")
     public String getIt() {
         return "Hi there!";
     }
     
     @GET
     @Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
-    @Path("user")
+    @Path("users")
+    @ApiOperation(
+            value = "Get User",
+            notes = "Simple jaxb object in http get",
+            tags = "get",
+            httpMethod = "GET",
+            responseClass = "ramp.sample.swagger.types.User")
     public User getUser() {
     	User user = new User();
     	user.setEmailId("test.email.id@i.com");
