8454d4f4092a3d347fb5a655fa09826c9dd92522
Palaniappan, Rama <Rama_Palaniappan@intuit.com>
2014-08-07 15:50:45 +0530

    alm and crash reporter integrated

--- android/Chirper/app/src/main/java/ramp/chirper/android/ChirperApplication.java
+++ android/Chirper/app/src/main/java/ramp/chirper/android/ChirperApplication.java
@@ -4,6 +4,10 @@ import android.app.Application;
 
 import com.intuit.acra.TrafficType;
 import com.intuit.acra.sender.CrashReporterSender;
+import com.intuit.mobile.alm.Alm;
+import com.intuit.mobile.alm.AlmNotification;
+import com.intuit.mobile.alm.annotation.AlmInitializationInfo;
+import com.intuit.mobile.alm.dto.AlmCustomInitInfo;
 
 import org.acra.ACRA;
 import org.acra.annotation.ReportsCrashes;
@@ -11,16 +15,18 @@ import org.acra.annotation.ReportsCrashes;
 /**
  * Created by rpalaniappan on 17/07/14.
  */
+@AlmInitializationInfo(
+        url="https://applifecycle-e2e.platform.intuit.com/v1/notifications",
+        //this app's credential may not pushed to e2e
+        apiKey="preprdakyresm7eilK4nmZLE6xy88HlfMlwFLMTk", //get api key from service portal
+//        apiKey = "preprdakyresY2eGEQql7y915PvHlNIx17ybRdfy",
+//        offeringId="7855421242231653664", // get offering id for your app from service portal
+        offeringId = "Intuit.platform.crashreporter.chirperandroid")
 @ReportsCrashes(
-
-        //URL to post crashes
         formUri = "https://crashlog.platform.intuit.com/api/v1/crashes",
-//Intuit provided API Key, this is a test api key
-//Get API Key for your project from Intuit Services Portal
+//        formKey = "prdakyres1JF2Oeu7islVqM8zn7fpDBmZkgkytX0",//This app's prod key may not be pushed to prod
         formKey = "akyresYQJQLLRgBIoL119ucHzOfpY89OKNWXGDaR",
-        //Max number of retries
         maxNumberOfRequestRetries = 10,
-        //send crashes even for app that is not signed
         sendReportsInDevMode = true
 )
 public class ChirperApplication extends Application {
@@ -29,6 +35,7 @@ public class ChirperApplication extends Application {
         super.onCreate();
         ACRA.init(this);
         ACRA.getErrorReporter().setReportSender(
-                new CrashReporterSender(TrafficType.SANDBOX));    }
-
+                new CrashReporterSender(TrafficType.LIVE));
+        Alm.init(this);
+    }
 }
--- android/Chirper/app/src/main/java/ramp/chirper/android/TwitterSearchActivity.java
+++ android/Chirper/app/src/main/java/ramp/chirper/android/TwitterSearchActivity.java
@@ -1,12 +1,20 @@
 package ramp.chirper.android;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.intuit.mobile.alm.AlmNotification;
+import com.intuit.mobile.alm.dto.AlmCustomInitInfo;
 
 
 public class TwitterSearchActivity extends ActionBarActivity {
@@ -17,6 +25,13 @@ public class TwitterSearchActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_twitter);
+        Toast toast = Toast.makeText(getApplicationContext(),
+                    "Crashes are reported to CTO-Dev Crash Reporter",
+                    Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP,0, 0);
+        toast.show();
+
+        notifyAppLifecycleListener();
     }
 
 
@@ -44,9 +59,21 @@ public class TwitterSearchActivity extends ActionBarActivity {
         Intent intent = new Intent(this, TweetListActivity.class);
         EditText searchEditText = (EditText) findViewById(R.id.searchQuery);
         String query = searchEditText.getText().toString();
-        nullChecker.verify(query);
+        nullChecker.verify(query, getApplicationContext());
         intent.putExtra(Constants.TWITTER_SEARCH_QUERY, query);
         startActivity(intent);
     }
+
+    private void notifyAppLifecycleListener() {
+//        AlmNotification almNotifier = AlmNotification.getInstance(this, null);
+//        almNotifier.almNotify();
+        AlmCustomInitInfo almCustomInfo = new AlmCustomInitInfo();
+        almCustomInfo.setAuthId("sample-auth-id");
+        almCustomInfo.setRealmId("sample-realm-id");
+        almCustomInfo.setPngDeviceToken("sample-png-realm-id");
+        AlmNotification almNotifier = AlmNotification.getInstance(this, almCustomInfo);
+        almNotifier.almNotify();
+    }
+
 
 }
